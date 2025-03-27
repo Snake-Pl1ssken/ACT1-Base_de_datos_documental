3efd0eb9a29735fc6a31b9d23487810a1f667227
Snake-Pl1ssken <jorgeluisgarciaorbegoso@gmail.com>
2025-03-27 14:28:04 +0100

    insercion de startTime y finishTime ademas de conectar a la base de datos

--- Assets/Scripts/StatsManager.cs
+++ Assets/Scripts/StatsManager.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 using System.IO;
 using System.Text;
 using System;
-//using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Security.Policy;
 
 public class StatsManager : MonoBehaviour
 {    
@@ -16,20 +18,34 @@ public class StatsManager : MonoBehaviour
 
     };
     
-    public string serverAddress = "192.168.113.25:27017";
+    public string serverAddress = "mongodb://localhost:27017";
     public string database = "racer";
     public string collection = "races";
 
     Dictionary<int, Stat> gateToStat;
+    BsonElement e;
+    MongoClient connection;
+    BsonDocument doc;
+    static IMongoDatabase databaseMongo;
+    static IMongoCollection<BsonDocument> collectionMongo;
 
-    //MongoClient connection;
 
     // Start is called before the first frame update
     void Start()
     {
         gateToStat = new Dictionary<int, Stat>();
+        
 
         // Connect to database
+
+        connection = new MongoClient(serverAddress);
+        databaseMongo = connection.GetDatabase(database);
+        collectionMongo = databaseMongo.GetCollection<BsonDocument>(collection);
+
+        if (connection != null)
+        {
+            Debug.Log("Database conected");
+        }
     }
 
     // Update is called once per frame
@@ -41,6 +57,10 @@ public class StatsManager : MonoBehaviour
     public void OnRaceStarted()
     {
         // Create race and register start date in database
+        doc = new BsonDocument();
+        e = new BsonElement("StartTime", new BsonDateTime(DateTime.Now));
+        doc.Add(e);
+        collectionMongo.InsertOne(doc);
     }
 
     public void OnGatePassed(int index, float time, float speed)
@@ -60,6 +80,36 @@ public class StatsManager : MonoBehaviour
     public void OnRaceFinished()
     {
         Debug.Log("Race finished");
+        doc = new BsonDocument();
+        e = new BsonElement("FinishTime", new BsonDateTime(DateTime.Now));
+        doc.Add(e);
+        collectionMongo.InsertOne(doc);
+
+        Stat stats = new Stat();
+
+        //_id: ObjectId,
+        //startTime: Date,
+
+        //gates: [ {  gate: Int32,
+        //            time: (double)stats.time,
+        //            speed: (double)stats.speed
+        //            },
+        //            {
+        //                gate: Int32,
+        //                time: (double)stats.time,
+        //                speed: (double)stats.speed
+        //            },
+        //            {
+        //                gate: Int32,
+        //                time: (double)stats.time,
+        //                speed: (double)stats.speed
+        //            },
+        //            {
+        //                gate: Int32,
+        //                time: (double)stats.time,
+        //                speed: (double)stats.speed
+        //            }
+        //       ]
 
     }
 
@@ -70,11 +120,11 @@ public class StatsManager : MonoBehaviour
 
         int count = 0;
 
-        foreach(int key in gateToStat.Keys)
+        foreach (int key in gateToStat.Keys)
         {
             Debug.Log("Getting gate " + key);
             stats[key] = gateToStat[key];
-            count ++;
+            count++;
         }
 
         return count;
