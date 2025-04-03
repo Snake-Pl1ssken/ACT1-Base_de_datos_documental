dfbad30f2ca1ce55dbb33c0f51b7ee08007549d8
Snake-Pl1ssken <jorgeluisgarciaorbegoso@gmail.com>
2025-04-03 14:27:36 +0200

    falta recuperar los datos de la base de datos

--- Assets/Scripts/StatsManager.cs
+++ Assets/Scripts/StatsManager.cs
@@ -17,18 +17,21 @@ public class StatsManager : MonoBehaviour
         public float speed;
 
     };
-    
+    public Mesh arrowMesh;
+    public Material arrowMaterial;
     public string serverAddress = "mongodb://localhost:27017";
     public string database = "racer";
     public string collection = "races";
 
     Dictionary<int, Stat> gateToStat;
     BsonElement e;
+    BsonElement e2;
     MongoClient connection;
     BsonDocument doc;
+    BsonArray gatesArray;
     static IMongoDatabase databaseMongo;
     static IMongoCollection<BsonDocument> collectionMongo;
-
+    ObjectId idObjectId;
 
     // Start is called before the first frame update
     void Start()
@@ -51,16 +54,30 @@ public class StatsManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float angle = 0;
+        Vector3 position = new Vector3(0, 1f, 0);
 
+        for (int i = 0; i < 100; i++)
+        {
+            position += Vector3.forward * 1f;
+            angle += 1f;
+
+            Color c = arrowMaterial.color;
+
+            arrowMaterial.color = new Color(c.r, c.g, c.b, 1.0f - i / 100.0f);
+
+            Graphics.DrawMesh(arrowMesh, position, Quaternion.Euler(0, angle, 0) * Quaternion.Euler(-90, 0 , 180), arrowMaterial, 0);
+        }
     }
 
     public void OnRaceStarted()
     {
         // Create race and register start date in database
         doc = new BsonDocument();
-        e = new BsonElement("StartTime", new BsonDateTime(DateTime.Now));
-        doc.Add(e);
-        collectionMongo.InsertOne(doc);
+        e2 = new BsonElement("StartTime", new BsonDateTime(DateTime.Now));
+        //doc.Add(e);
+        //collectionMongo.InsertOne(doc);
+        gatesArray = new BsonArray();
     }
 
     public void OnGatePassed(int index, float time, float speed)
@@ -71,45 +88,40 @@ public class StatsManager : MonoBehaviour
         s.time = time;
         s.speed = speed;
 
-        gateToStat[index] = s;
+       gateToStat[index] = s;
 
         // Register gate in database
+
+        doc = new BsonDocument();
+        e = new BsonElement("time", s.time);
+        doc.Add(e);
+        e = new BsonElement("speed", s.speed);
+        doc.Add(e);
+        //collectionMongo.InsertOne(doc);
+        gatesArray.Add(doc);
+
 
     }
 
     public void OnRaceFinished()
     {
+
+        string nombre = "Race";
+       
         Debug.Log("Race finished");
         doc = new BsonDocument();
-        e = new BsonElement("FinishTime", new BsonDateTime(DateTime.Now));
+        e = e2;
         doc.Add(e);
+        doc.Add(nombre, gatesArray);
         collectionMongo.InsertOne(doc);
 
+        idObjectId = doc["_id"].AsObjectId; //BsonArray.AsBsonArray
+        //la bd a añadido un id (aqui guardar el id y luego recuperarlo)
+        //var id = doc.{ collectionMongo}
+        //idString = id;
+        doc.Clear();
+
         Stat stats = new Stat();
-
-        //_id: ObjectId,
-        //startTime: Date,
-
-        //gates: [ {  gate: Int32,
-        //            time: (double)stats.time,
-        //            speed: (double)stats.speed
-        //            },
-        //            {
-        //                gate: Int32,
-        //                time: (double)stats.time,
-        //                speed: (double)stats.speed
-        //            },
-        //            {
-        //                gate: Int32,
-        //                time: (double)stats.time,
-        //                speed: (double)stats.speed
-        //            },
-        //            {
-        //                gate: Int32,
-        //                time: (double)stats.time,
-        //                speed: (double)stats.speed
-        //            }
-        //       ]
 
     }
 
@@ -120,12 +132,12 @@ public class StatsManager : MonoBehaviour
 
         int count = 0;
 
-        foreach (int key in gateToStat.Keys)
-        {
-            Debug.Log("Getting gate " + key);
-            stats[key] = gateToStat[key];
-            count++;
-        }
+        //foreach (int key in gateToStat.Keys)
+        //{
+        //    Debug.Log("Getting gate " + key);
+        //    stats[key] = gateToStat[key];
+        //    count++;
+        //}
 
         return count;
     }
